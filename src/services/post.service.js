c3a3eaabf9b64093160f1ab86ae90982df7fd33b
Khalimudin <khalimudin@gmail.com>
2019-02-01 20:27:21 +0700

    using axios

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,42 +1,36 @@
-let apiUrl = 'https://id.techinasia.com/wp-json/techinasia/3.0/posts';
-if (process.env.NODE_ENV === 'production') {
-  apiUrl = 'http://cors.io/?' + apiUrl;
-}
+import axios from 'axios'
+
+const apiUrl = 'https://id.techinasia.com/wp-json/techinasia/3.0/posts';
 
 export const postService = {
   getPosts,
   getPost,
 };
-const nocors = {
-  mode: 'no-cors'
-};
 
 function getPosts(page = 1) {
-  const requestOptions = {
-    ...nocors,
-    method: 'GET'
-  };
+  // const requestOptions = {
+  //   method: 'GET'
+  // };
 
-  return fetch(`${apiUrl}?page=${page}`, requestOptions).then(handleResponse);
+  return axios.get(`${apiUrl}?page=${page}`).then(response => response.data).catch(error => Promise.reject(error));
 }
 
 function getPost(slug) {
-  const requestOptions = {
-    ...nocors,
-    method: 'GET'
-  };
+  // const requestOptions = {
+  //   method: 'GET'
+  // };
 
-  return fetch(`${apiUrl}/${slug}`, requestOptions).then(handleResponse);
+  return axios.get(`${apiUrl}/${slug}`).then(response => response.data).catch(error => Promise.reject(error));
 }
 
-function handleResponse(response) {
-  return response.text().then(text => {
-    const data = text && JSON.parse(text);
-    if (!response.ok) {
-      const error = (data && data.message) || response.statusText;
-      return Promise.reject(error);
-    }
+// function handleResponse(response) {
+//   return response.text().then(text => {
+//     const data = text && JSON.parse(text);
+//     if (!response.ok) {
+//       const error = (data && data.message) || response.statusText;
+//       return Promise.reject(error);
+//     }
 
-    return data;
-  });
-}
+//     return data;
+//   });
+// }
