70d38170f42cb3e246f6113481072a55322d3233
Khalimudin <khalimudin@gmail.com>
2019-02-01 19:08:11 +0700

    Updates

---
+++ precache-manifest.c7a123f6ffe716d87a7266f3e3272c0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "452b6598514f72b7724f",
+    "url": "/microTIA/static/js/runtime~main.452b6598.js"
+  },
+  {
+    "revision": "ba8cf8b7f7d8cf775291",
+    "url": "/microTIA/static/js/main.ba8cf8b7.chunk.js"
+  },
+  {
+    "revision": "9dff646efea42b15ad39",
+    "url": "/microTIA/static/js/1.9dff646e.chunk.js"
+  },
+  {
+    "revision": "ba8cf8b7f7d8cf775291",
+    "url": "/microTIA/static/css/main.f6ac1eb5.chunk.css"
+  },
+  {
+    "revision": "973cb877b3b7752a9e94644a8a724423",
+    "url": "/microTIA/index.html"
+  }
+];
